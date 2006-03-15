197eaa134a4e168652f950eef71ac9a1b30016df
Andrew Uselton <uselton2@llnl.gov>
2006-03-15 18:47:11 +0000

    Added slurm.h and slurm.c

---
+++ slurm.c
@@ -0,0 +1,117 @@
+/*****************************************************************************\
+ *  $Id: slurm.h 1.13 2006/01/09 16:26:39 auselton Exp $
+ *****************************************************************************
+ *  Copyright (C) 2001-2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Andrew Uselton <uselton2@llnl.gov>
+ *  UCRL-CODE-2006-xxx.
+ *  
+ *  This file is part of Mib, an MPI-based parallel I/O benchamrk
+ *  For details, see <http://www.llnl.gov/linux/mib/>.
+ *  
+ *  Mib is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *  
+ *  Mib is distributed in the hope that it will be useful, but WITHOUT 
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ *  for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with Mib; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>     /* strncpy */
+#include <ctype.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include "sys_wrap.h"
+#include "slurm.h"
+
+void _get_str_env(char *str, char *key);
+int  _get_int_env(char *key);
+
+SLURM   *slurm;
+
+void
+get_SLURM_env()
+{
+  slurm = (SLURM*)Malloc(sizeof(SLURM));
+  slurm->CPUS_ON_NODE = _get_int_env("SLURM_CPUS_ON_NODE");
+  slurm->CPUS_PER_TASK = _get_int_env("SLURM_CPUS_PER_TASK");
+  _get_str_env(slurm->CPU_BIND_LIST, "SLURM_CPU_BIND_LIST");
+  _get_str_env(slurm->CPU_BIND_TYPE, "SLURM_CPU_BIND_TYPE");
+  _get_str_env(slurm->CPU_BIND_VERBOSE, "SLURM_CPU_BIND_VERBOSE");
+  slurm->JOBID = _get_int_env("SLURM_JOBID");
+  _get_str_env(slurm->LAUNCH_NODE_IPADDR, "SLURM_LAUNCH_NODE_IPADDR");
+  slurm->LOCALID = _get_int_env("SLURM_LOCALID");
+  slurm->NNODES = _get_int_env("SLURM_NNODES");
+  slurm->NODEID = _get_int_env("SLURM_NODEID");
+  _get_str_env(slurm->NODELIST, "SLURM_NODELIST");
+  slurm->NPROCS = _get_int_env("SLURM_NPROCS");
+  slurm->PROCID = _get_int_env("SLURM_PROCID");
+  _get_str_env(slurm->SRUN_COMM_HOST, "SLURM_SRUN_COMM_HOST");
+  slurm->SRUN_COMM_PORT = _get_int_env("SLURM_SRUN_COMM_PORT");
+  slurm->STEPID = _get_int_env("SLURM_STEPID");
+  _get_str_env(slurm->TASKS_PER_NODE, "SLURM_TASKS_PER_NODE");
+  slurm->TASK_PID = _get_int_env("SLURM_TASK_PID");
+  _get_str_env(slurm->UMASK, "SLURM_UMASK");
+  /*  show_SLURM_env(slurm); */
+}
+
+void
+show_SLURM_env()
+{
+  printf("SLURM_CPUS_ON_NODE = %d\n", slurm->CPUS_ON_NODE);
+  printf("SLURM_CPUS_PER_TASK = %d\n", slurm->CPUS_PER_TASK);
+  printf("SLURM_CPU_BIND_LIST = %s\n", slurm->CPU_BIND_LIST);
+  printf("SLURM_CPU_BIND_TYPE = %s\n", slurm->CPU_BIND_TYPE);
+  printf("SLURM_CPU_BIND_VERBOSE = %s\n", slurm->CPU_BIND_VERBOSE);
+  printf("SLURM_JOBID = %d\n", slurm->JOBID);
+  printf("SLURM_LAUNCH_NODE_IPADDR = %s\n", slurm->LAUNCH_NODE_IPADDR);
+  printf("SLURM_LOCALID = %d\n", slurm->LOCALID);
+  printf("SLURM_NNODES = %d\n", slurm->NNODES);
+  printf("SLURM_NODEID = %d\n", slurm->NODEID);
+  printf("SLURM_NODELIST = %s\n", slurm->NODELIST);
+  printf("SLURM_NPROCS = %d\n", slurm->NPROCS);
+  printf("SLURM_PROCID = %d\n", slurm->PROCID);
+  printf("SLURM_SRUN_COMM_HOST = %s\n", slurm->SRUN_COMM_HOST);
+  printf("SLURM_SRUN_COMM_PORT = %d\n", slurm->SRUN_COMM_PORT);
+  printf("SLURM_STEPID = %d\n", slurm->STEPID);
+  printf("SLURM_TASKS_PER_NODE = %s\n", slurm->TASKS_PER_NODE);
+  printf("SLURM_TASK_PID = %d\n", slurm->TASK_PID);
+  printf("SLURM_UMASK = %s\n", slurm->UMASK);
+}
+
+int
+_get_int_env(char *key)
+{
+  char *val;
+
+  if( (val = getenv(key)) != NULL )
+    {
+      /*      printf( "%s = %s\n", key, val);
+	      fflush(stdout);*/
+      return(atoi(val));
+    }
+  return(0);
+}
+  
+void
+_get_str_env(char *str, char *key)
+{
+  char *val;
+
+  if( (val = getenv(key)) != NULL )
+    {
+      strncpy(str, val, MAX_SLURM_BUF);
+    }
+  else
+    str[0] = '\0';
+}
+  
---
+++ slurm.h
@@ -0,0 +1,80 @@
+/*****************************************************************************\
+ *  $Id: slurm.h 1.13 2006/01/09 16:26:39 auselton Exp $
+ *****************************************************************************
+ *  Copyright (C) 2001-2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Andrew Uselton <uselton2@llnl.gov>
+ *  UCRL-CODE-2006-xxx.
+ *  
+ *  This file is part of Mib, an MPI-based parallel I/O benchamrk
+ *  For details, see <http://www.llnl.gov/linux/mib/>.
+ *  
+ *  Mib is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *  
+ *  Mib is distributed in the hope that it will be useful, but WITHOUT 
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ *  for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with Mib; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
+
+void get_SLURM_env();
+void show_SLURM_env();
+
+/* 
+ * These are the SLURM environment values available to a task.
+ * I got them with the command:
+ srun -N8 -n16 -l  bash -c set | grep SLURM
+ * The task 0 environment variables:
+ 00: SLURM_CPUS_ON_NODE=2
+ 00: SLURM_CPUS_PER_TASK=1
+ 00: SLURM_CPU_BIND_LIST=
+ 00: SLURM_CPU_BIND_TYPE=
+ 00: SLURM_CPU_BIND_VERBOSE=quiet
+ 00: SLURM_JOBID=66371
+ 00: SLURM_LABELIO=1
+ 00: SLURM_LAUNCH_NODE_IPADDR=192.168.17.198
+ 00: SLURM_LOCALID=0
+ 00: SLURM_NNODES=8
+ 00: SLURM_NODEID=0
+ 00: SLURM_NODELIST='adev[2,8-14]'
+ 00: SLURM_NPROCS=16
+ 00: SLURM_PROCID=0
+ 00: SLURM_SRUN_COMM_HOST=adevi
+ 00: SLURM_SRUN_COMM_PORT=3071
+ 00: SLURM_STEPID=0
+ 00: SLURM_TASKS_PER_NODE='2(x8)'
+ 00: SLURM_TASK_PID=9026
+ 00: SLURM_UMASK=0022
+ *
+ */
+#define MAX_SLURM_BUF 100
+
+typedef struct SLURM_Struct {
+  int CPUS_ON_NODE;
+  int CPUS_PER_TASK;
+  char CPU_BIND_LIST[MAX_SLURM_BUF];
+  char CPU_BIND_TYPE[MAX_SLURM_BUF];
+  char CPU_BIND_VERBOSE[MAX_SLURM_BUF];
+  int  JOBID;
+  char LAUNCH_NODE_IPADDR[MAX_SLURM_BUF];
+  int  LOCALID;
+  int  NNODES;
+  int  NODEID;
+  char NODELIST[MAX_SLURM_BUF];
+  int  NPROCS;
+  int  PROCID;
+  char SRUN_COMM_HOST[MAX_SLURM_BUF];
+  int  SRUN_COMM_PORT;
+  int  STEPID;
+  char TASKS_PER_NODE[MAX_SLURM_BUF];
+  int  TASK_PID;
+  char UMASK[MAX_SLURM_BUF];
+}SLURM;
+
