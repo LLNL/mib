b501bcb2c874e81f7fdfa2ff94e70760823b5b2c
Andrew Uselton <uselton2@llnl.gov>
2006-03-29 21:32:52 +0000

    Fleshed out the mpi_stub.h header for when there is not even any MPI stuff around.

---
+++ mpi_stubs.h
@@ -0,0 +1,42 @@
+/*****************************************************************************\
+ *  $Id: mpi_stubs.h,v 1.5 2005/11/30 20:24:57 auselton Exp $
+ *****************************************************************************
+ *  Copyright (C) 2001-2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Andrew Uselton <uselton2@llnl.gov>
+ *  UCRL-CODE-2006-xxx.
+ *  
+ *  This file is part of Mib, an MPI-based parallel I/O benchamrk
+ *  For details, see <http://www.llnl.gov/linux/mib/>.
+ *  
+ *  Mib is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *  
+ *  Mib is distributed in the hope that it will be useful, but WITHOUT 
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ *  for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with Mib; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
+
+typedef int MPI_Comm;
+typedef int MPI_Op;
+typedef int MPI_Datatype;
+typedef int MPI_Group;
+typedef int MPI_Errhandler;
+typedef int MPI_Status;
+/* MPI_Comm objects */
+#define MPI_COMM_WORLD ((MPI_Comm)1)
+#define MPI_COMM_NULL ((MPI_Comm)0)
+/* MPI_Datatype objects */
+#define MPI_INT 1
+#define MPI_DOUBLE 2
+/* MPI_Op objects */
+#define MPI_MAX 1
+#define MPI_MIN 1
+#define MPI_SUM 1
